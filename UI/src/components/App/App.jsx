75009bbd3329ed8f7d1a6d7985edb98973ab3151
Левыкин Алексей Александрович <a.a.levykin@gmail.com>
2023-12-24 10:06:06 +0500

    Добавлены методы

--- UI/src/components/App/App.jsx
+++ UI/src/components/App/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import ReactCompareImage from 'react-compare-image'
 import {
+  Alert,
   Backdrop,
-  Box,
   Button,
   Card,
   CardActions,
@@ -12,24 +12,36 @@ import {
   FormControlLabel,
   FormLabel,
   Grid,
+  IconButton,
   Radio,
   RadioGroup,
+  Snackbar,
   Typography
 } from '@mui/material'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload'
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark'
 import VisuallyHiddenInput from '../VisuallyHiddenInput/VisuallyHiddenInput'
 import beforeAsset from '../../assets/before.jpg'
 import afterAsset from '../../assets/after.jpg'
 
 const App = () => {
 
+  const [fileName, setFileName] = useState("")
+
   const [beforeSrc, setBeforeSrc] = useState(beforeAsset)
 
   const [afterSrc, setAfterSrc] = useState(afterAsset)
 
-  const [isVertical, setVerticalCompare] = useState(true)
+  const [isVertical, setVerticalCompare] = useState(false)
+
+  const [method, setMethod] = useState("x")
 
   const [inProgress, setInProgress] = useState(false)
+
+  const [isAlertOpen, setAlertOpen] = useState(false)
+
+  const [alertMsg, setAlertMsg] = useState("Метод находится в разработке")
 
   const fetchImage = async url => {
     const response = await fetch(url)
@@ -44,6 +56,69 @@ const App = () => {
     setInProgress(false)
   }
 
+  const picWish = (imgBefore) => {
+    fetch(imgBefore)
+      .then(response => response.blob())
+      .then(blob => {
+        const data = new FormData()
+        data.append('image_file', blob)
+        data.append('type', 'clean')
+        data.append('sync', 1)
+        const request = {
+          method: 'POST',
+          headers: {
+            'X-API-KEY': 'wxu89ab6exjvx2w08',
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+          },
+          body: data,
+        }
+
+        fetch('https://techhk.aoscdn.com/api/tasks/visual/scale', request)
+          .then(response => response.json())
+          .then(respData => setImages(respData.data.image, imgBefore))
+      })
+  }
+
+  const runOpenCV = (imgBefore) => {
+    
+    const leftImage = document.querySelector('[data-testid="left-image"]')
+    const tmpImg = new Image()
+    const tmpCanvas = document.createElement('canvas')
+    const cvsAfter = document.createElement('canvas')
+    const ocv = window.cv
+    
+    leftImage.parentNode.appendChild(tmpImg)
+    tmpImg.width = leftImage.width
+    tmpImg.height = leftImage.height      
+    tmpImg.src = imgBefore
+    tmpImg.id = 'tmpImg'
+
+    leftImage.parentNode.appendChild(tmpCanvas)
+    tmpCanvas.id = 'tmpCanvas'
+    tmpCanvas.width = leftImage.width
+    tmpCanvas.height = leftImage.height    
+    const tmpCtx = tmpCanvas.getContext('2d')
+    tmpCtx.drawImage(tmpImg, 0, 0)
+
+    let src = ocv.imread('tmpCanvas')
+    // let dst = new ocv.Mat()
+    // ocv.medianBlur(src, dst, 5)
+    // ocv.imshow(cvsAfter, dst)
+    // dst.delete()
+    // src.delete()
+
+    setAfterSrc(cvsAfter.toDataURL())
+    setBeforeSrc(tmpCanvas.toDataURL())
+
+    console.log(leftImage.width + ' ' + leftImage.height)
+    leftImage.parentNode.removeChild(tmpImg)
+    leftImage.parentNode.removeChild(tmpCanvas)
+   
+    setInProgress(false)
+  }
+
   const handleUpload = (e) => {
     const files = e.target.files
     if (files.length === 1) {
@@ -51,34 +126,49 @@ const App = () => {
       setInProgress(true)
 
       const imgBefore = URL.createObjectURL(files[0])
+      setFileName(files[0].name)
 
-      fetch(imgBefore)
-        .then(response => response.blob())
-        .then(blob => {
-          const data = new FormData()
-          data.append('image_file', blob)
-          data.append('type', 'clean')
-          data.append('sync', 1)
-          const request = {
-            method: 'POST',
-            headers: {
-              'X-API-KEY': 'wxu89ab6exjvx2w08',
-              'Accept': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-            },
-            body: data,
-          }
+      switch (method) {
+        case "x":
+          openAlert("Для работы с методом необходимо запустить локальный Web-сервер. Обратитесь к разработчику")          
+          break
+        case "p":
+          picWish(imgBefore)
+          break
+        case "o":
+          openAlert("Метод находится в разработке")
+          //runOpenCV(imgBefore)
+          break
+      }
+    }
+    e.target.value = ""
+  }
 
-          fetch('https://techhk.aoscdn.com/api/tasks/visual/scale', request)
-            .then(response => response.json())
-            .then(respData => setImages(respData.data.image, imgBefore))
-        })
+  const handleDownload = () => {
+    if (fileName !== "") {
+      let lnk = document.createElement('a')
+      lnk.download = fileName
+      lnk.href = afterSrc
+      lnk.click()
     }
   }
 
   const handleCompareDirChange = (e) => {
     setVerticalCompare(e.target.value === "v")
+  }
+
+  const handleMethodChange = (e) => {
+    setMethod(e.target.value)
+  }
+
+  const handleCloseAlert = () => {
+    setAlertOpen(false)
+  };
+
+  const openAlert = (msg) => {
+    setAlertMsg(msg)
+    setAlertOpen(true)
+    setInProgress(false)
   }
 
   return (
@@ -96,21 +186,48 @@ const App = () => {
               <Typography variant="body2" color="text.secondary" component="div" sx={{ pb: 2 }}>
                 Web-сервис для цветовой коррекции изображений.
               </Typography>
+              <hr />
               <FormControl>
                 <FormLabel>
-                  Сравнение
+                  <Typography variant="h5">
+                    Метод обработки
+                  </Typography>
                 </FormLabel>
-                <RadioGroup row defaultValue="v" onChange={handleCompareDirChange}>
+                <RadioGroup defaultValue="x" onChange={handleMethodChange}>
+                  <FormControlLabel value="x" control={<Radio />} label="X-MAS HACK 2023" />
+                  <FormControlLabel value="p" control={<Radio />} label="PicWish.com" />
+                  <FormControlLabel value="o" control={<Radio />} label="OpenCV.js" />
+                </RadioGroup>
+              </FormControl>
+              <hr />
+              <FormControl>
+                <FormLabel>
+                  <Typography variant="h5">
+                    Сравнение
+                  </Typography>
+                </FormLabel>
+                <RadioGroup row defaultValue="h" onChange={handleCompareDirChange}>
                   <FormControlLabel value="v" control={<Radio />} label="вертикальное" />
                   <FormControlLabel value="h" control={<Radio />} label="горизонтальное" />
                 </RadioGroup>
               </FormControl>
+              <hr />
             </CardContent>
-            <CardActions sx={{ height: '100%' }}>
-              <Button component="label" variant="contained" sx={{ width: '100%', mx: 2, mb: 2 }} startIcon={<CloudUploadIcon />}>
-                Загрузите файл с изображением
+            <CardActions>
+              <Button component="label" variant="outlined" sx={{ width: '100%', mx: 2, mb: 2 }} startIcon={<CloudUploadIcon />}>
+                Загрузить файл для обработки
                 <VisuallyHiddenInput type="file" accept="image/*" onChange={handleUpload} />
               </Button>
+            </CardActions>
+            <CardActions>
+              <Button component="label" variant="outlined" sx={{ width: '100%', mx: 2, mb: 2 }} startIcon={<CloudDownloadIcon />} onClick={handleDownload}>
+                Скачать обработанный файл
+              </Button>
+            </CardActions>
+            <CardActions>
+              <IconButton color="primary" size="small" sx={{ ml: 'auto', mr: 2 }}>
+                <QuestionMarkIcon />
+              </IconButton>
             </CardActions>
           </Grid>
         </Grid>
@@ -118,6 +235,11 @@ const App = () => {
       <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={inProgress}>
         <CircularProgress color="inherit" />
       </Backdrop>
+      <Snackbar open={isAlertOpen} autoHideDuration={5000} onClose={handleCloseAlert} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}>
+        <Alert severity="error" variant="filled" onClose={handleCloseAlert}>
+          {alertMsg}
+        </Alert>
+      </Snackbar>
     </>
   )
 }
--- UI/src/main.jsx
+++ UI/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { OpenCvProvider } from 'opencv-react'
 import App from './components/App/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <OpenCvProvider openCvPath='/opencv/opencv.js'>
+      <App />
+    </OpenCvProvider>
   </React.StrictMode>,
 )
